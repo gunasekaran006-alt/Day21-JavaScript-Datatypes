625ac87465f9b623e2d8bc307b777c37fe9a8aea
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-02-17 18:49:03 +0530

    initial

---
+++ script.js
@@ -0,0 +1,64 @@
+// Primitive Types
+
+let userName = "Narasimhan";
+console.log(typeof userName); // string
+
+let age = 20;
+age = 26;
+console.log(typeof age); // number
+
+let isMentor = true;
+console.log(typeof isMentor); // boolean
+
+let salary = undefined;
+console.log(typeof salary); // undefined
+
+let bigInt = 1239761357163251235n;
+console.log(typeof bigInt); // bigint ( big integer)
+
+// Real-time Example
+
+let email = "narasimhan123@mail.com";
+console.log(typeof email);
+
+let mobilenumber = 9010036699999;
+console.log(typeof mobilenumber);
+
+let userLogged = true;
+console.log(typeof userLogged);
+
+let paymentStatus;
+console.log(typeof paymentStatus);
+
+// Reference Data Types
+// object, array, function
+
+// array
+// let skills = "HTML, CSS, Bootstrap, JavaScript";
+let skills = ["HTML", "CSS", "Bootstrap", "JavaScript", 25];
+console.log(skills[2]);
+console.log(Array.isArray(skills));
+
+// function
+function welcome(){
+    console.log("Hey User You're Logged in");
+    console.log("You can access your courses");
+}
+welcome();
+
+let userProfile = {
+    username: "Lakshmi Narasimhan",
+    accountNumber: 9997131834123,
+    branch: "Tamil nadu",
+    country: "India",
+    accountActive: true,
+    accountBalance: undefined,
+    accountType: ['Salary Account', 'Personal Account']
+};
+
+let updateAccount = userProfile;
+updateAccount.branch = "Kumbakonam";
+
+// console.log(typeof userProfile);
+console.log(userProfile.accountNumber);
+console.log(userProfile);
